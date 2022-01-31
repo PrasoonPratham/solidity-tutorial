914fb41a987b878337bf619e1f041e2c2fc3e426
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2022-01-31 08:27:32 +0530

    description added

--- scripts/start.js
+++ scripts/start.js
@@ -3,11 +3,16 @@ async function main() {
   const keyboardsContract = await keyboardsContractFactory.deploy();
   await keyboardsContract.deployed();
 
-  console.log("Contract deployed to:", keyboardsContract.address);
+  let keyboards = await keyboardsContract.getKeyboards();
+  console.log("We got the keyboards!", keyboards);
 
-  const keyboards = await keyboardsContract.getKeyboards();
+  const keyboardTxn = await keyboardsContract.create("A really great keyboard!");
+  await keyboardTxn.wait();
+
+  keyboards = await keyboardsContract.getKeyboards();
   console.log("We got the keyboards!", keyboards);
 }
+
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
